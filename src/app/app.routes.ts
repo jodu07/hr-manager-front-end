e6cc85fe9d5a45d1a40c7c5719c95e78fc89e835
jodu07 <jhonnat.dussan@gmail.com>
2025-12-11 15:10:30 +0100

    guards added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { TASK_ROUTES } from './features/tasks/tasks.routes';
+import { authGuard } from './core/guard/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
@@ -7,6 +8,7 @@ export const routes: Routes = [
   { path: 'tasks', children: TASK_ROUTES },
   {
     path: 'admin',
+    canActivate: [authGuard],
     loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
   },
   {
