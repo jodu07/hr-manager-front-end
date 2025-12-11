dd4392f47be49830d94ece38b137b715fe7fc61b
jodu07 <jhonnat.dussan@gmail.com>
2025-12-11 13:13:32 +0100

    Login component created

---
+++ src/app/features/login/login.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {}
